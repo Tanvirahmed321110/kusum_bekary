ed15ad3491dca7324afe60c2ea3eaf05d9066560
tanvir321110 <tanvirahmed321110@gmail.com>
2025-05-15 12:46:37 +0600

    update cart

--- static/js/index.js
+++ static/js/index.js
@@ -5,3 +5,12 @@ const cartButton = document.getElementById('cart-btn')
 cartButton.addEventListener('click', function () {
     document.getElementById('cart-sidebar').classList.toggle('active')
 })
+
+
+const myCartLaptopBtn = document.getElementById('my-cart-laptop')
+
+if (myCartLaptopBtn) {
+    myCartLaptopBtn.addEventListener('click', function () {
+        document.getElementById('cart-sidebar').classList.toggle('laptop')
+    })
+}
